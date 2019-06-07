54f9885c990b0a97674b75a66c08d46b7aa00e81
Luiz dos Reis <luizdosreis@gmail.com>
2019-06-06 21:36:12 -0300

    map with key

--- src/app.js
+++ src/app.js
@@ -2,12 +2,19 @@
 
 import React, { Component } from 'react'
 import Title from './Title'
+import Square from './Square'
 
 class App extends Component {
   render () {
     return (
       <div>
         <Title />
+        {
+          ['green', 'blue', 'black'].map(color => (
+            <Square key={color} color={color} />
+          ))
+        }
+        <Square />
       </div>
     )
   }
