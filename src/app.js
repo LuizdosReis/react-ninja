531519ef5b10bd6f341a09605381a8cdf6d5cda8
Luiz dos Reis <luizdosreis@gmail.com>
2019-06-11 22:32:38 -0300

    changed the state

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import Title from './Title'
 import Square from './Square'
 import LikeButton from './LikeButton'
 import SearchButton from './SearchButton'
+import Button from './Button'
 
 class App extends Component {
   constructor () {
@@ -22,6 +23,7 @@ class App extends Component {
     return (
       <div>
         <Title title={title} />
+        <Button handleClick={() => { this.setState({ title: 'new Title' }) }}>change title</Button>
         {
           ['green', 'blue', 'black'].map(color => (
             <Square key={color} color={color} ><p>Using children</p></Square>
