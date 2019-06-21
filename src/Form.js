503517d59ee5d204be1caec902812e1b30fef265
Luiz dos Reis <luizdosreis@gmail.com>
2019-06-21 00:30:37 -0300

    converted Form to use hooks

--- src/Form.js
+++ src/Form.js
@@ -1,30 +1,23 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Input from './Input'
 import Checkbox from './Checkbox'
 
-class Form extends Component {
-  constructor () {
-    super()
-    this.state = {
-      value: 'Start value',
-      checked: false
-    }
-  }
+const Form = () => {
+  const [value, setValue] = useState('Start value')
+  const [checked, setChecked] = useState(false)
 
-  render () {
-    return (
-      <div>
-        <Input
-          value={this.state.value}
-          onChange={(e) => { this.setState({ value: e.target.value }) }}
-        />
-        <Checkbox
-          checked={this.state.checked}
-          onChange={() => { this.setState((prevState) => ({ checked: !prevState.checked })) }}
-        />
-      </div>
-    )
-  }
+  return (
+    <div>
+      <Input
+        value={value}
+        onChange={(e) => { setValue(e.target.value) }}
+      />
+      <Checkbox
+        checked={checked}
+        onChange={() => { setChecked(!checked) }}
+      />
+    </div>
+  )
 }
 
 export default Form
